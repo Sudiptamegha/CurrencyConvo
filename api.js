5d577424d7eafff491439682b9e3c816b4c78df4
sudiptadas2717 <156228802+sudiptadas2717@users.noreply.github.com>
2024-03-10 18:08:56 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,16 @@
+const BASE_URL =
+  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+const dropdowns = document.querySelectorAll(".dropdown select");
+
+// for (let select of dropdowns) {
+//   for (currCode in countryList) {
+//     let newOption = document.createElement("option");
+//     newOption.innerText = currCode;
+//     newOption.value = currCode;
+//     select.append(newOption);
+
+for (code in countryList) {
+  console.log(code, countryList[code]);
+}
+//   }
+// }
